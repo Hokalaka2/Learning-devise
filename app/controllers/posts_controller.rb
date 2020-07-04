4f5b23feca14be9d85592a1b0d7527fae043bfb6
Otis Milliken <otis.true@gmail.com>
2020-07-04 10:41:18 -0400

    added and configured devise

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
+
+    def index
+        @posts = Post.all
+    end
+    def new 
+        @post = Post.new
+    end
+    def create
+        @post = Post.new(params[:id])
+        @post.save
+        redirect_to root_path
+    end
+
+end
