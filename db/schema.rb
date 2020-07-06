8c8d1fcf6ec9a80357470a0ac6e8a1300b83f3f8
Otis Milliken <otis.true@gmail.com>
2020-07-06 10:25:29 -0400

    stylization, edited index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_04_141152) do
+ActiveRecord::Schema.define(version: 2020_07_04_181929) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_07_04_141152) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["memberstatus"], name: "index_users_on_memberstatus"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
